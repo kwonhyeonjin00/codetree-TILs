6890207ca6106af7abc30ab7ec6d326bd6673cea
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 07:53:48 +0900

    Title: 2개씩 그룹짓기, Time: 166ms, Memory: 39MB, Status: Wrong Answer - CodeTree

--- 241004/2개씩 그룹짓기/group-of-pairs.py
+++ 241004/2개씩 그룹짓기/group-of-pairs.py
@@ -1,19 +1,16 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr.sort()
-
 sum = 0
-
 for x in arr:
-    sum += x
-sum /= n
+    sum += x/n
 
 arr2 = []
 
 for i in range(n*2-1):
     for j in range(i+1, n*2):
-        if arr[i] + arr[j] >= sum:
-            arr2.append(arr[i] + arr[j])
+        x = arr[i] + arr[j]
+        if x >= sum:
+            arr2.append(x)
 
 print(min(arr2))
