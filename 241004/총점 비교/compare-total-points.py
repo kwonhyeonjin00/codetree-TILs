8960fa612d01df6e2b5780eaaa7700e0a6fb1477
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 10:15:56 +0900

    Title: 총점 비교, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241004/총점 비교/compare-total-points.py
@@ -0,0 +1,19 @@
+class People:
+    def __init__(self, name, kor, eng, math):
+        self.name = name
+        self.kor = kor
+        self.eng = eng
+        self.math = math
+
+n = int(input())
+
+people = []
+for i in range(n):
+    name, kor, eng, math = input().split()
+    kor, eng, math = int(kor), int(eng), int(math)
+    people.append(People(name, kor, eng, math))
+
+people.sort(key=lambda x: x.kor + x.eng + x.math)
+
+for x in people:
+    print(f'{x.name} {x.kor} {x.eng} {x.math}')
