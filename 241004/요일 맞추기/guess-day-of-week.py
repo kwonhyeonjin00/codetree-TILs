57a603757cd55391fcaeb37296bb5e1774b8097e
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 11:52:43 +0900

    Title: 요일 맞추기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241004/요일 맞추기/guess-day-of-week.py
+++ 241004/요일 맞추기/guess-day-of-week.py
@@ -1,35 +1,22 @@
-def f(a, b, c, d, t):
+def f(a, b, c, d):
+    day1 = b
+    day2 = d
 
-    elapsed_days = 0
-    
     #                  1.  2.  3.  4.  5.  6.  7.  8.  9. 10. 11. 12.
     num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
-    while True:
-        if a == c and b == d:
-            break
+    for i in range(1, a):
+        day1 += num_of_days[i]
 
-        elapsed_days += 1
-        b += 1
+    for j in range(1, c):
+        day2 += num_of_days[j]
 
-        if b > num_of_days[a]:
-            a += 1
-            b = 1
-    if t == 0:
-        print(days[elapsed_days % 7])
-    elif t == 1:
-        print(days[-(elapsed_days % 7)])
+    diff = day1 - day2
+    diff %= 7
+
+    print(days[7 - diff])
+
 
 m1, d1, m2, d2 = map(int, input().split())
-
-if m1 > m2:
-    f(m2, d2, m1, d1, 1)
-
-elif m1 == m2:
-    if d1 > d2:
-        f(m2, d2, m1, d1, 1)
-    else:
-        f(m1, d1, m2, d2, 0)
-else:
-    f(m1, d1, m2, d2, 0)
+f(m1, d1, m2, d2)
