595a43f6fd7d2cf1ea7a1c3f34946acf36d276e0
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 10:53:32 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 161ms, Memory: 25MB, Status: Passed - CodeTree

--- 241004/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
+++ 241004/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -12,9 +12,6 @@ for i in range(n):
 
 sequence.sort(key=lambda x: x.num)
 
-for x in sequence:
-    print(f'{x.num} {x.number}')
-
 for i in range(1, n+1):
     for j in range(n):
         if sequence[j].number == i:
