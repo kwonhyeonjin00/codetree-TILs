41039e3fec36b31ee447053610967743198f3422
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-05 07:06:42 +0900

    Title: 연속되는 수 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241005/연속되는 수 2/continuous-number2.py
+++ 241005/연속되는 수 2/continuous-number2.py
@@ -1,17 +1,24 @@
-n = int(input())
-arr = []
-for i in range(n):
-    arr.append(int(input()))
-print(arr)
-lens = []
-cnt = 1
+def f(n):
+    arr = []
+    for i in range(n):
+        arr.append(int(input()))
 
-for i in range(n):
-    if i == 0:
-        cnt += 1
-    elif arr[i] == arr[i - 1]:
-        cnt += 1
-    else:
-        lens.append(cnt)
-        cnt = 1
-print(max(lens))
+    lens = []
+    cnt = 1
+
+    for i in range(n):
+        if i == 0:
+            cnt += 1
+        elif arr[i] == arr[i - 1]:
+            cnt += 1
+        else:
+            lens.append(cnt)
+            cnt = 1
+    print(max(lens))
+
+n = int(input())
+
+if n == 1:
+    print('1')
+else:
+    f(n)
