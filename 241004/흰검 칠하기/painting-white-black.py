1d0707bbbb109fc8befda41ca0d26d8277c9e8f6
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 17:36:53 +0900

    Title: 흰검 칠하기, Time: 176ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241004/흰검 칠하기/painting-white-black.py
+++ 241004/흰검 칠하기/painting-white-black.py
@@ -1,44 +1,33 @@
-MAX_K = 100000
-# 변수 선언 및 입력:
 n = int(input())
-a = [0] * (2 * MAX_K + 1)
-cnt_b = [0] * (2 * MAX_K + 1)
-cnt_w = [0] * (2 * MAX_K + 1)
-b, w, g = 0, 0, 0
+arr = [[0, '0'] for _ in range(n*198+1)]
 
-cur = MAX_K
-for _ in range(n):
-    x, c = tuple(input().split())
+idx = n*99
+
+for i in range(n):
+    x, t = input().split()
     x = int(x)
+    if t == 'R':
+        for j in range(idx, idx+x):
+            arr[j][0] += 1
+            arr[j][1] = 'b'
 
-    if c == 'L':
-        # x칸 왼쪽으로 칠합니다.
-        while x > 0:
-            a[cur] = 1
-            cnt_w[cur] += 1
-            x -= 1
+        idx += x-1
+    elif t == 'L':
+        for j in range(idx, idx-x, -1): 
+            arr[j][0] += 1
+            arr[j][1] = 'w'
+        idx -= x-1
 
-            if x: 
-                cur -= 1
-    else:
-        # x칸 오른쪽으로 칠합니다.
-        while x > 0:
-            a[cur] = 2
-            cnt_b[cur] += 1
-            x -= 1
+cnt_w = 0
+cnt_b = 0
+cnt_g = 0
 
-            if x: 
-                cur += 1
+for i in arr:
+    if i[0] >= 4:
+        cnt_g += 1
+    elif i[1] == 'w':
+        cnt_w += 1
+    elif i[1] == 'b':
+        cnt_b += 1
 
-for i in range(2 * MAX_K + 1):
-    # 검은색과 흰색으로 두 번 이상 칠해진 타일은 회색입니다.
-    if cnt_b[i] >= 2 and cnt_w[i] >= 2: 
-        g += 1
-    # 그렇지 않으면 현재 칠해진 색깔이 곧 타일의 색깔입니다.
-    elif a[i] == 1: 
-        w += 1
-    elif a[i] == 2: 
-        b += 1
-
-# 정답을 출력합니다.
-print(w, b, g)
+print(cnt_w, cnt_b, cnt_g)
