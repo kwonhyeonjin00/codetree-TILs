32c540d9e3cca6568a47aab68bbdce0e57bc4b5e
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 15:09:59 +0900

    Title: 흰검 칠하기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241004/흰검 칠하기/painting-white-black.py
+++ 241004/흰검 칠하기/painting-white-black.py
@@ -1,40 +1,43 @@
 n = int(input())
-arr = [0 for _ in range(n*10+1)]
+arr = [[0, '0'] for _ in range(n*2+1)]
 
-idx = n*5
+idx = n
 
 for i in range(n):
     x, t = input().split()
     x = int(x)
     if t == 'R':
-        for j in range(idx, idx+x):
-            if arr[j] == 5:
+        for j in range(idx+1, idx+x+1):
+            if arr[j][1] == 'g':
                 continue
-            else:
-                arr[j] += 1
+            elif arr[j][0] == 3:
+                arr[j][1] = 'g'
+            
+            arr[j][0] += 1
+            arr[j][1] = 'b'
+
         idx += x
     elif t == 'L':
-        for j in range(idx-1, idx-x-1, -1):
-            if arr[j] == 3:
-                arr[j] = 5
-            elif arr[j] == 5:
+        for j in range(idx, idx-x, -1):
+            if arr[j][1] == 'g':
                 continue
-            elif arr[j] == 0:
-                arr[j] = 2
-            else:
-                arr[j] += 1
+            elif arr[j][0] == 3:
+                arr[j][1] = 'g'
+            
+            arr[j][0] += 1
+            arr[j][1] = 'w'
         idx -= x 
-
+    print(arr)
 cnt_w = 0
 cnt_b = 0
 cnt_g = 0
 
 for i in arr:
-    if i == 2 or i == 4:
+    if i[1] == 'w':
         cnt_w += 1
-    elif i == 1 or i == 3:
+    elif i[1] == 'b':
         cnt_b += 1
-    elif i == 5:
+    elif i[1] == 'g':
         cnt_g += 1
 
 print(cnt_w, cnt_b, cnt_g)
