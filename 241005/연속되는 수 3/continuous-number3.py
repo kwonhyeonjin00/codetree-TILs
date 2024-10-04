bd0bf9c28fb0e669766a5eefe53c379d83dca3b7
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-05 07:20:17 +0900

    Title: 연속되는 수 3, Time: 120ms, Memory: 25MB, Status: Passed - CodeTree

--- 241005/연속되는 수 3/continuous-number3.py
+++ 241005/연속되는 수 3/continuous-number3.py
@@ -1,23 +1,13 @@
-def f(n):
-    arr = []
-    for i in range(n):
-        arr.append(int(input()))
-
-    lens = []
-    cnt = 0
-
-    for i in range(n):
-        if i == 0 or arr[i] * arr[i - 1] > 0:
-            cnt += 1
-        else:
-            lens.append(cnt)
-            cnt = 1
-    lens.append(cnt)
-    print(max(lens))
-
 n = int(input())
+arr = [int(input()) for _ in range(n)]
 
-if n == 1:
-    print('1')
-else:
-    f(n)
+cnt, ans = 0, 0
+for i in range(n):
+    if i >= 1 and arr[i] * arr[i - 1] > 0:
+        cnt += 1
+    else:
+        cnt = 1
+
+    ans = max(ans, cnt)
+
+print(ans)
