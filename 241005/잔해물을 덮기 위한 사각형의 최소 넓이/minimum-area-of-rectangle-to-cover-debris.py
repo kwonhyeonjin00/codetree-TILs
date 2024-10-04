9fc6761f6eb61d0ef1ea71044b3ddd0048ed38ec
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-05 06:00:19 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 195ms, Memory: 55MB, Status: Wrong Answer - CodeTree

--- 241005/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 241005/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -9,15 +9,15 @@ arr = [[0 for _ in range(2000)] for _ in range(2000)]
 x1, y1, x2, y2 = map(int, input().split())
 x1, y1, x2, y2 = x1+1000, y1+1000, x2+1000, y2+1000
 
-for j in range(x1, x2+1):
-    for k in range(y1, y2+1):
+for j in range(x1, x2):
+    for k in range(y1, y2):
         arr[j][k] = 1
 
 x1, y1, x2, y2 = map(int, input().split())
 x1, y1, x2, y2 = x1+1000, y1+1000, x2+1000, y2+1000
 
-for j in range(x1, x2+1):
-    for k in range(y1, y2+1):
+for j in range(x1, x2):
+    for k in range(y1, y2):
         arr[j][k] = 0
 
 xt1, yt1 = check(arr)
@@ -26,5 +26,5 @@ for i in range(2000):
     for j in range(2000):
         if arr[i][j] == 1:
             xt2, yt2 = i, j
-
-print((xt2 - xt1) * (yt2 - yt1))
+print(xt1, yt1, xt2, yt2)
+print((xt2 - xt1+1) * (yt2 - yt1+1))
