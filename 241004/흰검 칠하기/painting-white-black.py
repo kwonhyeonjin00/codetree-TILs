1b62e4abdf58a64c21b823e02565a7e6c4a3043e
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 17:27:44 +0900

    Title: 흰검 칠하기, Time: 141ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 241004/흰검 칠하기/painting-white-black.py
+++ 241004/흰검 칠하기/painting-white-black.py
@@ -8,24 +8,14 @@ for i in range(n):
     x = int(x)
     if t == 'R':
         for j in range(idx, idx+x):
-            if arr[j][1] == 'g':
-                continue
-            elif arr[j][0] == 3:
-                arr[j][1] = 'g'
-            else:
-                arr[j][0] += 1
-                arr[j][1] = 'b'
+            arr[j][0] += 1
+            arr[j][1] = 'b'
 
         idx += x-1
     elif t == 'L':
-        for j in range(idx, idx-x, -1):
-            if arr[j][1] == 'g':
-                continue
-            elif arr[j][0] == 3:
-                arr[j][1] = 'g'
-            else:
-                arr[j][0] += 1
-                arr[j][1] = 'w'
+        for j in range(idx, idx-x, -1): 
+            arr[j][0] += 1
+            arr[j][1] = 'w'
         idx -= x-1
 
 cnt_w = 0
@@ -33,11 +23,11 @@ cnt_b = 0
 cnt_g = 0
 
 for i in arr:
-    if i[1] == 'w':
+    if i[0] >= 4:
+        cnt_g += 1
+    elif i[1] == 'w':
         cnt_w += 1
     elif i[1] == 'b':
         cnt_b += 1
-    elif i[1] == 'g':
-        cnt_g += 1
 
 print(cnt_w, cnt_b, cnt_g)
