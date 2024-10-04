55ebc827d21e1684d1f9a38206e18aa58d40f744
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 10:53:00 +0900

    Title: 정렬된 숫자 위치 알아내기, Time: 105ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241004/정렬된 숫자 위치 알아내기/indices-of-sorted-array.py
@@ -0,0 +1,22 @@
+class Sequence:
+    def __init__(self, num, number):
+        self.num = num
+        self.number = number
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+sequence = []
+for i in range(n):
+    sequence.append(Sequence(arr[i], i+1))
+
+sequence.sort(key=lambda x: x.num)
+
+for x in sequence:
+    print(f'{x.num} {x.number}')
+
+for i in range(1, n+1):
+    for j in range(n):
+        if sequence[j].number == i:
+            print(j+1, end=' ')
+            break
