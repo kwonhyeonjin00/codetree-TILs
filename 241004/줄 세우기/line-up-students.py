612e20e5e29722218e093457e2b385f65bfb2022
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-04 10:26:06 +0900

    Title: 줄 세우기, Time: 190ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241004/줄 세우기/line-up-students.py
@@ -0,0 +1,17 @@
+class People:
+    def __init__(self, height, weight, number):
+        self.height = height
+        self.weight = weight
+        self.number = number
+
+n = int(input())
+
+people = []
+for i in range(n):
+    height, weight = map(int, input().split())
+    people.append(People(height, weight, i+1))
+
+people.sort(key=lambda x: (-x.height, -x.weight, x.number))
+
+for x in people:
+    print(f'{x.height} {x.weight} {x.number}')
