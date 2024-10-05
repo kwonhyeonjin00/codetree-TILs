51974c610ce7be0c3e206f615953f1ee45cbc66c
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-05 11:04:15 +0900

    Title: 벌금은 누구에게, Time: 165ms, Memory: 25MB, Status: Passed - CodeTree

--- 241005/벌금은 누구에게/who-will-pay.py
+++ 241005/벌금은 누구에게/who-will-pay.py
@@ -10,12 +10,15 @@ n, m, k = map(int, input().split())
 arr = [0 for _ in range(n)]
 checking = 0
 
-while checking == 0:
+while True:
+    if checking == 1 or m == 0:
+        break 
     x = int(input())
     arr[x-1] += 1
     t = check(arr)
     if t != 0:
         print(t)
+    m -= 1
 
 if checking == 0:
     print(-1)
