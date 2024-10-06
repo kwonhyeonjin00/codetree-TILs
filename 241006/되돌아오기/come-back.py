d57bb9c472cb2c58f50e89544ad82f8aac357367
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-06 19:41:00 +0900

    Title: 되돌아오기, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241006/되돌아오기/come-back.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+dxs, dys = [1, 0, -1, 0], [0, -1, 0, 1]
+
+direction = {
+    'E': 0,
+    'S': 1,
+    'W': 2,
+    'N': 3
+}
+x, y = 0, 0
+time = 0
+
+for i in range(n):
+    d, s = input().split()
+    s = int(s)
+
+    dir_num = direction[d]
+
+    for j in range(s):
+        x, y = x + dxs[dir_num], y + dys[dir_num]
+        time += 1
+
+        if x == 0 and y == 0:
+            print(time)
+            break
