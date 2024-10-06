c7df09aa9ad6087698a9537799df0110d574ddf1
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-06 20:09:59 +0900

    Title: 되돌아오기, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 241006/되돌아오기/come-back.py
+++ 241006/되돌아오기/come-back.py
@@ -12,25 +12,21 @@ x, y = 0, 0
 time = 0
 check = 0
 
-def checking(x, y, t):
-    if x == 0 and y == 0:
-        print(t)
-        x, y = 1001, 1001
-
 for i in range(n):
     d, s = input().split()
     s = int(s)
 
     dir_num = direction[d]
-
+    
     for j in range(s):
         x, y = x + dxs[dir_num], y + dys[dir_num]
         time += 1
-
         if x == 0 and y == 0:
-            checking(x, y, time)
+            print(time)
             check = 1
             break
+    if check == 1:
+        break
 
 if check == 0:
     print(-1)
