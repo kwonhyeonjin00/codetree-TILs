58214e871fd12f383d5ea1b9184f177d18c03cdd
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-08 18:37:39 +0900

    Title: 오목, Time: 114ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241008/오목/O-mok.py
+++ 241008/오목/O-mok.py
@@ -1,12 +1,16 @@
 arr = [list(map(int, input().split())) for _ in range(19)]
 def check(n):
-    for i in range(15):
+    for i in range(19):
         for j in range(15):
             if arr[i][j] == n and arr[i][j + 1] == n and arr[i][j + 2] == n and arr[i][j + 3] == n and arr[i][j + 4] == n:
                 return i + 1, j + 3
-            elif arr[i][j] == n and arr[i + 1][j] == n and arr[i + 2][j] == n and arr[i + 3][j] == n and arr[i + 4][j] == n:
+    for i in range(15):
+        for j in range(19):
+            if arr[i][j] == n and arr[i + 1][j] == n and arr[i + 2][j] == n and arr[i + 3][j] == n and arr[i + 4][j] == n:
                 return i + 3, j + 1
-            elif arr[i][j] == n and arr[i + 1][j + 1] == n and arr[i + 2][j + 2] == n and arr[i + 3][j + 3] == n and arr[i + 4][j + 4] == n:
+    for i in range(15):
+        for j in range(4, 19):
+            if arr[i][j] == n and arr[i + 1][j + 1] == n and arr[i + 2][j + 2] == n and arr[i + 3][j + 3] == n and arr[i + 4][j + 4] == n:
                 return i + 3, j + 3
     for i in range(15):
         for j in range(4, 19):
