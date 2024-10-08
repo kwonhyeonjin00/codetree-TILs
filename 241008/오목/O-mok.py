08627b28d2b14094c5b8420aa241f9484a6eeb9d
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-08 18:31:15 +0900

    Title: 오목, Time: 138ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241008/오목/O-mok.py
@@ -0,0 +1,24 @@
+arr = [list(map(int, input().split())) for _ in range(19)]
+def check(n):
+    for i in range(15):
+        for j in range(15):
+            if arr[i][j] == n and arr[i][j + 1] == n and arr[i][j + 2] == n and arr[i][j + 3] == n and arr[i][j + 4] == n:
+                return i + 1, j + 3
+            elif arr[i][j] == n and arr[i + 1][j] == n and arr[i + 2][j] == n and arr[i + 3][j] == n and arr[i + 4][j] == n:
+                return i + 3, j + 1
+            elif arr[i][j] == n and arr[i + 1][j + 1] == n and arr[i + 2][j + 2] == n and arr[i + 3][j + 3] == n and arr[i + 4][j + 4] == n:
+                return i + 3, j + 3
+    return -1, -1
+
+
+x, y = check(1)
+if x != -1:
+    print("1")
+    print(x, y)
+else:
+    x, y = check(2)
+    if x != -1:
+        print("2")
+        print(x, y)
+    else:
+        print(0)
