594d13428156878377fb9843c18857aa16ae0089
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-08 16:15:27 +0900

    Title: 이상한 진수 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 241008/이상한 진수 2/awkward-digits-2.py
+++ 241008/이상한 진수 2/awkward-digits-2.py
@@ -1,15 +1,25 @@
+def two(n):
+    num = 0
+    t = len(n) - 1
+    for i in range(t+1):
+        num += 2 ** t * int(n[i]) 
+        t -= 1
+    return num
+
 s = list(input())
+
+ans = 0
 
 for i in range(len(s)):
     if s[i] == '0':
         s[i] = '1'
-        break
+        t = two(s)
+        ans = max(ans, t)
+        s[i] = '0'
+    elif s[i] == '1':
+        s[i] = '0'
+        t = two(s)
+        ans = max(ans, t)
+        s[i] = '1'
 
-n = 0
-t = len(s) - 1
-
-for i in s:
-    n += 2 ** t * int(i)
-    t -= 1
-
-print(n)
+print(ans)
