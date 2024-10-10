65ff84800070a5c8fb852f64f674079b342b636e
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-10 18:33:00 +0900

    Title: G or H 3, Time: 2909ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 241010/G or H 3/G-or-H-3.py
@@ -0,0 +1,21 @@
+max_num = 100000
+
+arr = [0] * max_num
+n, k = map(int, input().split())
+
+for i in range(n):
+    x, c = input().split()
+    x = int(x)
+
+    arr[x] = 1 if c == 'G' else 2
+
+ans = 0
+
+for i in range(max_num - k):
+    t = 0
+    for j in range(i, i + k + 1):
+        t += arr[j]
+
+    ans = max(ans, t)
+
+print(ans)
