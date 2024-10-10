f00ea87a5306747252d40139a182523b63d4d12b
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-10 20:44:05 +0900

    Title: 아름다운 수열 2, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 241010/아름다운 수열 2/beautiful-sequence-2.py
+++ 241010/아름다운 수열 2/beautiful-sequence-2.py
@@ -2,18 +2,15 @@ n, m = map(int, input().split())
 arr1 = list(map(int, input().split()))
 arr2 = list(map(int, input().split()))
 
-x = ""
-for i in arr2:
-    x += str(i)
+arr2.sort()
 
 cnt = 0
 
 for i in range(n - m + 1):
-    t = 0
-    for j in range(i, i + m):
-        if str(arr1[j]) in x:
-            t += 1
-        if t == m:
-            cnt += 1
+    t = arr1[i:i+m]
+    t.sort()
+    if t == arr2:
+        cnt += 1
+
 
 print(cnt)
