941c1178b70cf020c2a279f0b4859bc8bb21f122
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-13 12:50:47 +0900

    Title: 두 직사각형, Time: 165ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241013/두 직사각형/two-rectangles.py
@@ -0,0 +1,9 @@
+x1, y1, x2, y2 = map(int, input().split())
+a1, b1, a2, b2 = map(int, input().split())
+
+string = "overlapping"
+
+if a1 > x2 or x1 > a2 or b1 > y2 or y1 > b2:
+    string = "nonoverlapping"
+
+print(string)
