1202d88512df6a25efc218382be94bc613fd1c3a
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 22:48:52 +0900

    Title: 세 수의 최대 곱, Time: 101ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 241014/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,11 +1,43 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-x = -10000000000
-for i in range(n - 2):
-    for j in range(i + 1, n - 1):
-        for k in range(j + 1, n):
-            t = arr[i] * arr[j] * arr[k]
+arr0 = []
+arr1 = []
 
-            x = max(x, t)
-print(x)
+for i in arr:
+    if i >= 0:
+        arr0.append(i)
+    else:
+        arr1.append(i)
+
+if arr0:
+    t1 = max(arr0)
+    arr0.pop(arr0.index(t1))
+else:
+    t1 = -1001
+
+if arr0:
+    t2 = max(arr0)
+    arr0.pop(arr0.index(t2))
+else:
+    t2 = -1001
+
+if arr0:
+    t3 = max(arr0)
+    arr0.pop(arr0.index(t3))
+else:
+    t3 = -1001
+
+if arr1:
+    t4 = min(arr1)
+    arr1.pop(arr1.index(t4))
+else:
+    t4 = -1001
+
+if arr1:
+    t5 = min(arr1)
+    arr1.pop(arr1.index(t5))
+else:
+    t5 = -1001
+
+print(max(t1 * t2 * t3, t1 * t4 * t5))
