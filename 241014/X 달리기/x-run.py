15d2a06bd783ffddb11136910bd7387d46d272b2
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 14:06:20 +0900

    Title: X 달리기, Time: 118ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241014/X 달리기/x-run.py
+++ 241014/X 달리기/x-run.py
@@ -4,6 +4,12 @@ def min_move(n):
         t += i
     return t
 
+def max_move(n):
+    t = 0
+    for i in range(n + 2):
+        t += i
+    return t
+
 n = int(input())
 
 s = 0
@@ -15,16 +21,16 @@ while True:
         break
     
     s += v
+    time += 1
 
     move = min_move(v)
     tx = n - s - move
 
     if tx > v:
-        v += 1
+        if max_move(v) <= tx:
+            v += 1
     elif tx < v:
         if v > 1:
             v -= 1
-
-    time += 1
 
 print(time)
