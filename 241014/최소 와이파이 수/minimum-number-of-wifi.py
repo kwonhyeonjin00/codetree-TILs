e0effaa19fc1cc7a9edb274986ecc2b7a0611d1d
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 23:53:02 +0900

    Title: 최소 와이파이 수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241014/최소 와이파이 수/minimum-number-of-wifi.py
+++ 241014/최소 와이파이 수/minimum-number-of-wifi.py
@@ -1,20 +1,22 @@
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 
-t = 2 * m + 1
+if m != 0:
+    t = 2 * m + 1
+    cnt = 0
 
-cnt = 0
+    while True:
+        if arr:
+            if arr[0] == 0:
+                arr.pop(0)
+            elif arr[0] == 1:
+                for i in range(t):
+                    arr.pop(0)
+                cnt += 1
+        else:
+            break
 
-while True:
-    if arr:
-        if arr[0] == 0:
-            arr.pop(0)
-        elif arr[0] == 1:
-            arr.pop(0)
-            arr.pop(0)
-            arr.pop(0)
-            cnt += 1
-    else:
-        break
+    print(cnt)
 
-print(cnt)
+else:
+    print(arr.count(1))
