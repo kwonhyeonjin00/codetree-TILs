11fd151f3741a19c142005085862511d6dcc6595
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 18:34:14 +0900

    Title: 독서실의 거리두기 2, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/독서실의 거리두기 2/study-cafe-keeping-distance-2.py
@@ -0,0 +1,21 @@
+n = int(input())
+arr = list(input())
+
+arr_index = []
+diff = []
+for i in range(n):
+    if arr[i] == '1':
+        arr_index.append(i)
+
+if arr_index[0] != 0:
+    diff.append(arr_index[0])
+
+for j in range(len(arr_index) - 1):
+    x = arr_index[j + 1] - arr_index[j]
+    if x != 1:
+        diff.append((arr_index[j + 1] - arr_index[j]) // 2)
+
+if arr_index[-1] != n - 1:
+    diff.append(n - 1 - arr_index[-1])
+
+print(min(diff))
