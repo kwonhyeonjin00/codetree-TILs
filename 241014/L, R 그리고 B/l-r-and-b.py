0621135f91f9418c232b7d7d3e749755139f1b68
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 21:56:46 +0900

    Title: L, R 그리고 B, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/L, R 그리고 B/l-r-and-b.py
@@ -0,0 +1,10 @@
+arr = [list(input()) for _ in range(10)]
+
+for i in range(10):
+    for j in range(10):
+        if arr[i][j] == 'B':
+            Bx, By = i, j
+        elif arr[i][j] == 'L':
+            Lx, Ly = i, j
+
+print(abs(Bx - Lx) + abs(Lx - Ly) - 1)
