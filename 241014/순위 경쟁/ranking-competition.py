c3c9de3f0dd48fe525aa9c101a3843c31117e225
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 22:26:57 +0900

    Title: 순위 경쟁, Time: 129ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/순위 경쟁/ranking-competition.py
@@ -0,0 +1,26 @@
+n = int(input())
+first = [0, 0, 0]
+score = [0, 0, 0]
+
+cnt = 0
+
+for i in range(n):
+    fir = [0, 0, 0]
+    c, s = input().split()
+    s = int(s)
+    if c == "A":
+        score[0] += s
+    elif c == "B":
+        score[1] += s
+    elif c == "C":
+        score[2] += s
+
+    for i in range(3):
+        if score[i] == max(score):
+            fir[i] = 1
+
+    if first != fir:
+        cnt += 1
+        first = fir
+
+print(cnt)
