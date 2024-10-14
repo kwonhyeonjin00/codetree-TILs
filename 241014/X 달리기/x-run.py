fa2c5599e08d6949ed2b953571f429c73a87b007
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 13:37:09 +0900

    Title: X 달리기, Time: 121ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/X 달리기/x-run.py
@@ -0,0 +1,29 @@
+def min_move(n):
+    t = 0
+    for i in range(n):
+        t += i
+    return t
+
+n = int(input())
+
+s = 0
+v = 1
+time = 0
+
+while True:
+    if s == n:
+        break
+    
+    s += v
+
+    move = min_move(v)
+    tx = n - s - move
+
+    if tx > v:
+        v += 1
+    elif tx < v:
+        v -= 1
+
+    time += 1
+
+print(time)
