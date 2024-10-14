26b1d57f478b49fd830fb3ad737fa53cf09677d2
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-14 23:00:03 +0900

    Title: 세 수의 최대 곱, Time: 130ms, Memory: 31MB, Status: Passed - CodeTree

--- 241014/세 수의 최대 곱/maximum-product-of-three-numbers.py
+++ 241014/세 수의 최대 곱/maximum-product-of-three-numbers.py
@@ -1,48 +1,23 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-arr0 = []
-arr1 = []
+t1 = max(arr)
+arr.pop(arr.index(t1))
 
-for i in arr:
-    if i >= 0:
-        arr0.append(i)
+t2 = max(arr)
+arr.pop(arr.index(t2))
+
+t3 = max(arr)
+arr.pop(arr.index(t3))
+
+if arr:
+    t4 = min(arr)
+    arr.pop(arr.index(t4))
+    if arr:
+        t5 = min(arr)
+        arr.pop(arr.index(t5))
+        print(max(t1 * t2 * t3, t1 * t4 * t5))
     else:
-        arr1.append(i)
-
-if arr0:
-    t1 = max(arr0)
-    arr0.pop(arr0.index(t1))
+        print(t1 * t2 * t3) 
 else:
-    t1 = -1001
-
-if arr0:
-    t2 = max(arr0)
-    arr0.pop(arr0.index(t2))
-else:
-    t2 = -1001
-
-if arr0:
-    t3 = max(arr0)
-    arr0.pop(arr0.index(t3))
-else:
-    t3 = -1001
-
-if arr1:
-    t4 = min(arr1)
-    arr1.pop(arr1.index(t4))
-else:
-    t4 = -1001
-
-if arr1:
-    t5 = min(arr1)
-    arr1.pop(arr1.index(t5))
-else:
-    t5 = -1001
-
-if t1 == -1001:
-    t6 = min(arr1)
-    arr1.pop(arr1.index(t6))
-else:
-    t6 = t1
-print(max(t1 * t2 * t3, t4 * t5 * t6))
+    print(t1 * t2 * t3)
