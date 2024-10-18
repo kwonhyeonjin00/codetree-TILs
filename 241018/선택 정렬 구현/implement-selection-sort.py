5e03a3fb1e822c25af1ac1dd87b7ac7008f4ffcc
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-18 15:42:37 +0900

    Title: 선택 정렬 구현, Time: 111ms, Memory: 25MB, Status: Passed - CodeTree

--- 241018/선택 정렬 구현/implement-selection-sort.py
+++ 241018/선택 정렬 구현/implement-selection-sort.py
@@ -1,9 +1,9 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-for i in range(n - 1):
+for i in range(n):
     min_idx = i
-    for j in range(i + 1, n - 1):
+    for j in range(i + 1, n):
         if arr[j] < arr[min_idx]:
             min_idx = j
 
