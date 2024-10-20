2064c4e43918820ab12149b4ba59e7792eb74993
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-21 02:05:39 +0900

    Title: 정수 명령 처리, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241021/정수 명령 처리/process-numeric-commands.py
@@ -0,0 +1,39 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+
+    def empty(self):
+        if len(self.items) == 0:
+            return 1
+        else:
+            return 0
+
+    def top(self):
+        return self.items[-1]
+
+n = int(input())
+
+stack = Stack()
+
+for i in range(n):
+    x = input().split()
+
+    if x[0] == 'push':
+        stack.push(int(x[1]))
+    elif x[0] == 'pop':
+        print(stack.pop())
+    elif x[0] == 'size':
+        print(stack.size())
+    elif x[0] == 'empty':
+        print(stack.empty())
+    elif x[0] == 'top':
+        print(stack.top)
