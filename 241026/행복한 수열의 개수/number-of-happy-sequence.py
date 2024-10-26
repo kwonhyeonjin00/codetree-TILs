ed781e1570f0a10836252e20866175e741cf5c94
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-26 21:42:16 +0900

    Title: 행복한 수열의 개수, Time: 125ms, Memory: 25MB, Status: Passed - CodeTree

--- 241026/행복한 수열의 개수/number-of-happy-sequence.py
+++ 241026/행복한 수열의 개수/number-of-happy-sequence.py
@@ -1,42 +1,40 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-ans = 0
+if m == 1:
+    print(2 * n)
 
-for i in range(n):
-    x = 1
-    temp = arr[i][0]
-    if x == m:
-        ans += 1
-        continue
+else:
+    ans = 0
 
-    for j in range(1, n):
-        if temp == arr[i][j]:
-            x += 1
-        else:
-            x = 1
-            temp = arr[i][j]
+    for i in range(n):
+        x = 1
+        temp = arr[i][0]
 
-        if x == m:
-            ans += 1
-            break
+        for j in range(1, n):
+            if temp == arr[i][j]:
+                x += 1
+            else:
+                x = 1
+                temp = arr[i][j]
 
-for i in range(n):
-    x = 0
-    temp = arr[0][i]
-    if x == m:
-        ans += 1
-        continue
+            if x == m:
+                ans += 1
+                break
 
-    for j in range(n):
-        if temp == arr[j][i]:
-            x += 1
-        else:
-            x = 0
-            temp = arr[j][i]
-        
-        if x == m:
-            ans += 1
-            break
+    for i in range(n):
+        x = 1
+        temp = arr[0][i]
 
-print(ans)
+        for j in range(1, n):
+            if temp == arr[j][i]:
+                x += 1
+            else:
+                x = 1
+                temp = arr[j][i]
+            
+            if x == m:
+                ans += 1
+                break
+
+    print(ans)
