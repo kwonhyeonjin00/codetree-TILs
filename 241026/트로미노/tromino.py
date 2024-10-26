eec7a43ecdca4674d20bc8ff949f41910549567d
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-26 21:52:48 +0900

    Title: 트로미노, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241026/트로미노/tromino.py
+++ 241026/트로미노/tromino.py
@@ -1,33 +1,33 @@
 n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for _ in range(n)]
 
-ans1 = 0
+ans = 0
 
 for i in range(n - 2):
     for j in range(n):
         temp = arr[i][j] + arr[i+1][j] + arr[i+2][j]
-        ans1 = max(ans1, temp)
+        ans = max(ans, temp)
 
 for i in range(n):
     for j in range(n - 2):
         temp = arr[i][j] + arr[i][j+1] + arr[i][j+2]
-        ans1 = max(ans1, temp)
-
+        ans = max(ans, temp)
+print(ans)
 for i in range(n - 1):
     for j in range(n - 1):
         temp = arr[i][j] + arr[i+1][j] + arr[i+1][j+1]
-        ans1 = max(ans1, temp)
+        ans = max(ans, temp)
 
         temp -= arr[i][j]
         temp += arr[i][j+1]
-        ans1 = max(ans1, temp)
+        ans = max(ans, temp)
 
         temp -= arr[i+1][j]
         temp += arr[i][j]
-        ans1 = max(ans1, temp)
+        ans = max(ans, temp)
 
         temp -= arr[i+1][j+1]
         temp += arr[i+1][j]
-        ans1 = max(ans1, temp)
+        ans = max(ans, temp)
 
-print(ans1)
+print(ans)
