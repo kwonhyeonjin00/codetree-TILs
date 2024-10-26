f713f6e6d84ed745a3c7ce60b3ea171aa6f49871
kwonhyeonjin00 <kahj3749@naver.com>
2024-10-26 21:55:33 +0900

    Title: 트로미노, Time: 181ms, Memory: 25MB, Status: Passed - CodeTree

--- 241026/트로미노/tromino.py
+++ 241026/트로미노/tromino.py
@@ -4,17 +4,17 @@ arr = [list(map(int, input().split())) for _ in range(n)]
 ans = 0
 
 for i in range(n - 2):
-    for j in range(n):
+    for j in range(m):
         temp = arr[i][j] + arr[i+1][j] + arr[i+2][j]
         ans = max(ans, temp)
 
 for i in range(n):
-    for j in range(n - 2):
+    for j in range(m - 2):
         temp = arr[i][j] + arr[i][j+1] + arr[i][j+2]
         ans = max(ans, temp)
-print(ans)
+
 for i in range(n - 1):
-    for j in range(n - 1):
+    for j in range(m - 1):
         temp = arr[i][j] + arr[i+1][j] + arr[i+1][j+1]
         ans = max(ans, temp)
 
