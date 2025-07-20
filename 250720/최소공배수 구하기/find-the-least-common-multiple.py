fcc30aeb94abae6cf582b87a55b58b46b9c9a7d2
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-20 14:14:48 +0900

    Title: 최소공배수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250720/최소공배수 구하기/find-the-least-common-multiple.py
+++ 250720/최소공배수 구하기/find-the-least-common-multiple.py
@@ -2,7 +2,7 @@ n, m = map(int, input().split())
 
 # Please write your code here.
 def lcm(n, m):
-    x = 0
+    x = 1
     for i in range(2, min(n, m) + 1):
         if n % i == 0 and m % i == 0:
             x = i
