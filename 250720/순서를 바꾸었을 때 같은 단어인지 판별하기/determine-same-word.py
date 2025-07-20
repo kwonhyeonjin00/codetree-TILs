623276548a3ced8064a92f7b2aaa1aeb39558f9d
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-20 20:37:03 +0900

    Title: 순서를 바꾸었을 때 같은 단어인지 판별하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250720/순서를 바꾸었을 때 같은 단어인지 판별하기/determine-same-word.py
@@ -0,0 +1,19 @@
+word1 = input()
+word2 = input()
+
+# Please write your code here.
+word1 = sorted(word1)
+word2 = sorted(word2)
+c = 1
+
+if len(word1) != len(word2):
+    print("No")
+else:
+    for i in range(len(word1)):
+        if word1[i] != word2[i]:
+            print("No")
+            c = 0
+            break
+
+if c == 1:
+    print("Yes")
