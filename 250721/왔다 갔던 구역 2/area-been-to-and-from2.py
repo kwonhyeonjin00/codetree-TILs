7fff1eeddd1f6c897054c040beb3138130bcf1fa
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-21 22:32:08 +0900

    Title: 왔다 갔던 구역 2, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250721/왔다 갔던 구역 2/area-been-to-and-from2.py
@@ -0,0 +1,28 @@
+n = int(input())
+x = []
+dir = []
+for _ in range(n):
+    xi, di = input().split()
+    x.append(int(xi))
+    dir.append(di)
+
+# Please write your code here.
+stack = [0 for _ in range(2000)]
+
+pos = 1000
+
+for i in range(n):
+    for j in range(x[i]):
+        stack[pos] += 1
+
+        if dir[i] == 'R':
+            pos += 1
+        else:
+            pos -= 1
+cnt = 0
+
+for k in range(2000):
+    if stack[k] >= 2:
+        cnt += 1
+
+print(cnt)
