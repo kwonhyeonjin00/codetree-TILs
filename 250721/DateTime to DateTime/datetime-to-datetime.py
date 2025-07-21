cb30976208e68e505602c304e9809e5bdd5faf4e
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-21 20:16:12 +0900

    Title: DateTime to DateTime, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250721/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,13 @@
+a, b, c = map(int, input().split())
+
+# Please write your code here.
+def cal(a, b, c):
+    m = (b * 60 + c) - (11 * 60 + 11)
+    d = a - 11
+
+    return d * 1440 + m
+
+if b <= 10 or (b == 11 and c <= 10):
+    print(-1)
+else:
+    print(cal(a, b, c))
