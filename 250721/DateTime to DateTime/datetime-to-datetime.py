39ab29201b973bd3b80c17d500b2a5882ff8e4e1
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-21 20:17:39 +0900

    Title: DateTime to DateTime, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/DateTime to DateTime/datetime-to-datetime.py
+++ 250721/DateTime to DateTime/datetime-to-datetime.py
@@ -7,7 +7,7 @@ def cal(a, b, c):
 
     return d * 1440 + m
 
-if a != 11 and (b <= 10 or (b == 11 and c <= 10)):
+if a == 11 and (b <= 10 or (b == 11 and c <= 10)):
     print(-1)
 else:
     print(cal(a, b, c))
