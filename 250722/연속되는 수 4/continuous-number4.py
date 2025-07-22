b13345abf2e2a3e0466498993166685185bda04b
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-22 22:48:34 +0900

    Title: 연속되는 수 4, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250722/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,18 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+# Please write your code here.
+cnt = 0
+ans = 1
+
+for i in range(0, n):
+
+    if i >= 1 and arr[i] > arr[i - 1]:
+        cnt += 1
+
+    else:
+        cnt = 1
+
+    ans = max(ans, cnt)
+
+print(ans)
