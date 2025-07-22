cf5854ac3d3bd11d99083c754d65cd577467b60d
kwonhyeonjin00 <kahj3749@naver.com>
2025-07-22 10:28:25 +0900

    Title: 계속 중첩되는 사각형, Time: 88ms, Memory: 20MB, Status: Runtime Error - Codetree

--- 250722/계속 중첩되는 사각형/continuously-overlapping-squares.py
+++ 250722/계속 중첩되는 사각형/continuously-overlapping-squares.py
@@ -21,7 +21,7 @@ def paint_blue(d_x1, d_y1, d_x2, d_y2):
         for j in range(d_y1, d_y2):
             check[i][j] = 2
 
-for i in range(0, n // 2 + 1, 2):
+for i in range(0, n, 2):
     paint_red(x1[i], y1[i], x2[i], y2[i])
     paint_blue(x1[i + 1], y1[i + 1], x2[i + 1], y2[i + 1])
 
