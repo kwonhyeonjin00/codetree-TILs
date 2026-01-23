52c92d270398c855c0fa1f2a5296406db74949c1
kwonhyeonjin00 <kahj3749@naver.com>
2026-01-23 23:16:45 +0900

    Title: 최고의 33위치, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

--- 260123/최고의 33위치/best-place-of-33.py
+++ 260123/최고의 33위치/best-place-of-33.py
@@ -10,6 +10,6 @@ for i in range(n - 2):
         cnt = 0    
         cnt += grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] + grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] 
 
-    maxx = max(maxx, cnt)
+        maxx = max(maxx, cnt)
 
 print(maxx)
