9f49ad2f12458c4f61c7d7990843f8766a1c2174
kwonhyeonjin00 <kahj3749@naver.com>
2026-01-23 23:16:02 +0900

    Title: 최고의 33위치, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260123/최고의 33위치/best-place-of-33.py
+++ 260123/최고의 33위치/best-place-of-33.py
@@ -5,8 +5,9 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 maxx = 0
 
 for i in range(n - 2):
-    cnt = 0
-    for j in range(n - 2):      
+    
+    for j in range(n - 2):  
+        cnt = 0    
         cnt += grid[i][j] + grid[i][j + 1] + grid[i][j + 2] + grid[i + 1][j] + grid[i + 1][j + 1] + grid[i + 1][j + 2] + grid[i + 2][j] + grid[i + 2][j + 1] + grid[i + 2][j + 2] 
 
     maxx = max(maxx, cnt)
