2f40e7353dfdd0b4578469658e96068107da5a4b
kwonhyeonjin00 <kahj3749@naver.com>
2026-04-23 23:12:43 +0900

    Title: 2차원 바람, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260423/2차원 바람/The-2D-wind-blows.py
+++ 260423/2차원 바람/The-2D-wind-blows.py
@@ -7,23 +7,22 @@ a = [list(map(int, input().split())) for _ in range(n)]
 winds = [tuple(map(int, input().split())) for _ in range(q)]
 
 # Please write your code here.
-for i in range(q):
-    r1, c1, r2, c2 = winds.pop(0)
+for r1, c1, r2, c2 in winds:
     r1, c1, r2, c2 = r1 - 1, c1 - 1, r2 - 1, c2 - 1
 
-    temp = a[r1].pop(c2)
-    for i in range(1, r2 - r1):
-        a[r1 + i].append(temp)
-        temp = a[r1 + i].pop(c2)
+    temp = a[r1][c1]
 
-    a[r2].insert(c2 + 1, temp)
 
-    temp = a[r2].pop(c1)
-    for i in range(1, r2 - r1):
-        a[r2 - i].insert(c1, temp)
-        temp = a[r2 - i].pop(c1 + 1)
+    for i in range(r1, r2):
+        a[i][c1] = a[i + 1][c1]
+    for i in range(c1, c2):
+        a[r2][i] = a[r2][i + 1]
+    for i in range(r2, r1, -1):
+        a[i][c2] = a[i - 1][c2]
+    for i in range(c2, c1 + 1, -1):
+        a[r1][i] = a[r1][i - 1]
 
-    a[r1].insert(c1, temp)
+    a[r1][c1 + 1] = temp
 
     b = [row[:] for row in a]
 
@@ -45,10 +44,7 @@ for i in range(q):
                 t += b[i][j + 1]
                 cnt += 1
             
-            t //= cnt
-            a[i][j] = t
+            a[i][j] = t // cnt
 
 for row in a:
-    for elem in row:
-        print(elem, end=' ')
-    print()
+    print(*(row))
