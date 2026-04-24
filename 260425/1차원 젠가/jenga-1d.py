f2dedcbf1a27386227ba3300024c2e56180f3cea
kwonhyeonjin00 <kahj3749@naver.com>
2026-04-25 00:33:49 +0900

    Title: 1차원 젠가, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260425/1차원 젠가/jenga-1d.py
+++ 260425/1차원 젠가/jenga-1d.py
@@ -6,8 +6,9 @@ s2, e2 = map(int, input().split())
 # Please write your code here.
 blocks = blocks[:s1-1] + blocks[e1:]
 blocks = blocks[:s2-1] + blocks[e2:]
-print(len(blocks))
+
 if blocks:
+    print(len(blocks))
     for x in blocks:
         print(x)
 else:
