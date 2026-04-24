dd55a86adc0a007c7b0173dd9b98ce64c77960ab
kwonhyeonjin00 <kahj3749@naver.com>
2026-04-24 23:54:48 +0900

    Title: 기울어진 직사각형의 회전, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
+++ 260424/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -4,11 +4,49 @@ r, c, m1, m2, m3, m4, dir = map(int, input().split())
 
 # Please write your code here.
 
-dx, dy = r, c
-if dir = 0:
-    dx, dy = r, c
-    temp = grid[dx][dy]
-    for i in range(1, m4 + 1):
-        
+dx, dy = r - 1, c - 1
 
-    
+if dir == 0:
+    temp = grid[dx][dy]
+
+    for i in range(1, m4 + 1):
+        dx, dy = dx - 1, dy - 1
+        grid[dx + 1][dy + 1] = grid[dx][dy]
+
+    for i in range(1, m3 + 1):
+        dx, dy = dx - 1, dy + 1
+        grid[dx + 1][dy - 1] = grid[dx][dy]
+
+    for i in range(1, m2 + 1):
+        dx, dy = dx + 1, dy + 1
+        grid[dx - 1][dy - 1] = grid[dx][dy]
+
+    for i in range(1, m1 + 1):
+        dx, dy = dx + 1, dy - 1
+        grid[dx - 1][dy + 1] = grid[dx][dy]
+
+    grid[dx - 1][dy + 1] = temp
+
+elif dir == 1:
+    temp = grid[dx][dy]
+
+    for i in range(1, m1 + 1):
+        dx, dy = dx - 1, dy + 1
+        grid[dx + 1][dy - 1] = grid[dx][dy]
+
+    for i in range(1, m2 + 1):
+        dx, dy = dx - 1, dy - 1
+        grid[dx + 1][dy + 1] = grid[dx][dy]
+
+    for i in range(1, m3 + 1):
+        dx, dy = dx + 1, dy - 1
+        grid[dx - 1][dy + 1] = grid[dx][dy]
+
+    for i in range(1, m4 + 1):
+        dx, dy = dx + 1, dy + 1
+        grid[dx - 1][dy - 1] = grid[dx][dy]
+
+    grid[dx - 1][dy - 1] = temp
+
+for row in grid:
+    print(*row)
