53f6ec061dbdf46423395c2250e3e5c05e09eec3
kwonhyeonjin00 <kahj3749@naver.com>
2026-04-24 23:34:32 +0900

    Title: 기울어진 직사각형의 회전, Time: 0ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 260424/기울어진 직사각형의 회전/rotate-slanted-rectangle.py
@@ -0,0 +1,14 @@
+n = int(input())
+grid = [list(map(int, input().split())) for _ in range(n)]
+r, c, m1, m2, m3, m4, dir = map(int, input().split())
+
+# Please write your code here.
+
+dx, dy = r, c
+if dir = 0:
+    dx, dy = r, c
+    temp = grid[dx][dy]
+    for i in range(1, m4 + 1):
+        
+
+    
