75b2f838c9a3b169b240426a0df4e92106887552
kwonhyeonjin00 <kahj3749@naver.com>
2026-04-25 00:31:15 +0900

    Title: 1차원 젠가, Time: 0ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 260425/1차원 젠가/jenga-1d.py
@@ -0,0 +1,13 @@
+n = int(input())
+blocks = [int(input()) for _ in range(n)]
+s1, e1 = map(int, input().split())
+s2, e2 = map(int, input().split())
+
+# Please write your code here.
+blocks = blocks[:s1-1] + blocks[e1:]
+blocks = blocks[:s2-1] + blocks[e2:]
+if blocks:
+    for x in blocks:
+        print(x)
+else:
+    print(0)
